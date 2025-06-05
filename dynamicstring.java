cec01b2eeb43139cc491f791f4df86e6bb9e9ec9
Gandla Aswini <aswinigandla86@gmail.com>
2025-06-05 15:32:37 +0530

    Add files via upload

---
+++ dynamicstring.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String a=new String("Aswini");
+        for(int i=0;i<a.length();i++)
+        {
+        System.out.println(a.charAt(i));
+        }
+    }
+}
